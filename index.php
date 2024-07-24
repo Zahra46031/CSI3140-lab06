dedc4cda1c81ccf1871ec9af4648abdc56c46783
Zahra Suleymanova <105732760+Zahra46031@users.noreply.github.com>
2024-07-23 21:08:06 -0400

    lab 7 changes

--- index.php
+++ index.php
@@ -28,3 +28,13 @@ $game->resetTurn();
 echo "Turn reset. Turn: " . $game->getTurn() . "<br>";
 $dice = $game->rollDice();
 echo "New Turn Roll 1: " . implode(", ", $dice) . "<br>";
+<div id="output"></div>
+<button id="version">Version</button>
+
+<script>
+const output = document.getElementById("output");
+const version = document.getElementById("version");
+version.onclick = function(e) {
+  output.innerHTML = "Look up version clicked";
+}
+</script>
